1e6e03c7a4ab3e2d02290d830e37d311b5ede2e3
joshualerickson <joshualerickson@gmail.com>
2021-04-19 08:29:36 -0600

    updated fire_risk_wkflw

--- fire_risk_wkflw.R
+++ fire_risk_wkflw.R
@@ -17,11 +17,19 @@ huc12_wcc$fire_risk_sum <- exact_extract(fire_risk, huc12_wcc, 'sum')
 
 #write for backup
 write_sf(huc12_wcc, 'huc12_wcc_fire_risk.gpkg', layer = 'fire_risk')
+fire_risk_sf <- read_sf( 'huc12_wcc_fire_risk.gpkg', layer = 'fire_risk') %>% select(-SHAPE_Length, -SHAPE_Area)
+
+#write for exploring in ArcGIS
+arc.write(path = 'T:/FS/NFS/R01/Program/7140Geometronics/GIS/Project/zz_R1WCC_Jan2021/Data/OutputDatasets_Review.gdb/WCC_HUC12_r1_FireRisk', data = fire_risk_sf,
+          validate = TRUE)
 
 #bring in final_wcc_reassessment
 wcc_add_fire_risk <- read_sf('T:/FS/NFS/R01/Program/7140Geometronics/GIS/Project/zz_R1WCC_Jan2021/Data/OutputDatasets_Review.gdb', layer = 'Final_WCC_Reassessment')
 
 
+
+
+st_layers('T:/FS/NFS/R01/Program/7140Geometronics/GIS/Project/zz_R1WCC_Jan2021/Data/OutputDatasets_Review.gdb/')
 ggplot() + geom_sf(data = huc12_wcc, aes(fill = log10(fire_risk_sum)))
 huc12_wcc %>% st_drop_geometry() %>%
   ggplot() + geom_histogram(aes(fire_risk_sum)) + scale_x_log10(labels  = scales::comma)
